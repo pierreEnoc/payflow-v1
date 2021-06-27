d012258f83d248d0fe13145d6f5a826a1719d4b8
Pierre enoc <pierre.enoc@gympass.com>
2021-06-27 10:15:50 -0300

    :sparkles: add app bar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
+        appBar: PreferredSize(
+      preferredSize: Size.fromHeight(152),
+      child: Container(
+        height: 152,
+        color: AppColors.primary,
+        child: Center(
+          child: ListTile(
+            title: Text.rich(
+              TextSpan(text: "Olá ", style: TextStyles.titleRegular, children: [
+                TextSpan(text: "Pierre", style: TextStyles.titleBoldBackground)
+              ]),
+            ),
+            subtitle: Text("Mantenha suas contas em dia",
+                style: TextStyles.captionShape),
+            trailing: Container(
+              height: 48,
+              width: 48,
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+        ),
       ),
-      
-    );
+    ));
   }
 }
