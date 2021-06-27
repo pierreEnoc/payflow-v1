8737cc3281e452785adaa5cc4f0a647270530fd1
Pierre enoc <pierre.enoc@gympass.com>
2021-06-27 19:05:32 -0300

    :sparkles: add app bar,buton navigation

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -8,29 +8,68 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-      preferredSize: Size.fromHeight(152),
-      child: Container(
-        height: 152,
-        color: AppColors.primary,
-        child: Center(
-          child: ListTile(
-            title: Text.rich(
-              TextSpan(text: "Olá ", style: TextStyles.titleRegular, children: [
-                TextSpan(text: "Pierre", style: TextStyles.titleBoldBackground)
-              ]),
-            ),
-            subtitle: Text("Mantenha suas contas em dia",
-                style: TextStyles.captionShape),
-            trailing: Container(
-              height: 48,
-              width: 48,
-              decoration: BoxDecoration(
-                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                    text: "Olá ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                          text: "Pierre", style: TextStyles.titleBoldBackground)
+                    ]),
+              ),
+              subtitle: Text("Mantenha suas contas em dia",
+                  style: TextStyles.captionShape),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
             ),
           ),
         ),
       ),
-    ));
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.home, color: AppColors.primary)),
+            GestureDetector(
+              onTap: () {
+                print("clicou");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
